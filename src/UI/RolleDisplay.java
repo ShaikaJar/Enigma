f00bc8b72018cf3f98445cd74dcaf8586aac79ca
Lorenz Umhöfer <lorenz@karolo.de>
2021-09-24 11:20:32 +0200

    Rollen können reseted werden

--- src/UI/RolleDisplay.java
+++ src/UI/RolleDisplay.java
@@ -1,27 +1,38 @@
 package UI;
 
 import Logic.Rolle;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 
 public class RolleDisplay extends Group {
 
     Button[] rollen = new Button[3];
+    
+    Button reset;
 
     RolleDisplay(int[] positionen){
-        double layoutX = 0;//this.getParent().getLayoutX();
-        double layoutY = 0;//this.getParent().getLayoutX();
-        this.setLayoutX(layoutX);
-        this.setLayoutY(layoutY);
         this.setStyle("-fx-background-color: #999999;");
-        for (int i = 0; i < rollen.length; i++) {
+        for (int i = 0; i < 3; i++) {
             rollen[i] = new Button(text(positionen[i]));
-            rollen[i].setLayoutX(layoutX+40*i);
-            rollen[i].setLayoutY(layoutY);
+            rollen[i].setLayoutX(40*i);
+            rollen[i].setLayoutY(0);
             rollen[i].setVisible(true);
             this.getChildren().add(rollen[i]);
         }
+
+
+        reset = new Button("Reset");
+        reset.setLayoutX(40*4);
+        reset.setLayoutY(0);
+        reset.setVisible(true);
+        this.getChildren().add(reset);
+    }
+    
+    public void setOnReset(EventHandler<MouseEvent> resetHandler){
+        reset.setOnMouseClicked(resetHandler);
     }
 
     private String text(int pos){
@@ -30,7 +41,7 @@ public class RolleDisplay extends Group {
 
 
     public void update(int[] positionen){
-        for (int i = 0; i < rollen.length; i++) {
+        for (int i = 0; i < 3; i++) {
             rollen[i].setText(text(positionen[i]));
         }
     }
