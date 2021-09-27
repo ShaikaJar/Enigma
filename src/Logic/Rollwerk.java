d6e3e500375fb7cafb7c1481db5d93f51905aa36
Lorenz <lorenz@karolo.de>
2021-09-27 21:05:34 +0200

    Rollwerk dokumentiert

--- src/Logic/Rollwerk.java
+++ src/Logic/Rollwerk.java
@@ -6,12 +6,23 @@ public class Rollwerk {
     private Rolle rolle3;
     private UmkehrWalze umkehrWalze = UmkehrWalze.UMKEHR_WALZE_A;
 
+    /**
+     * Ein Rollwerk mit drei ausgewählten Rollen, die in der angegebenen Reihenfolge zur Verschlüsselung benutzt werden
+     * @param rolle1 Erste Rolle
+     * @param rolle2 Zweite Rolle
+     * @param rolle3 Dritte Rolle
+     */
     public Rollwerk(Rolle rolle1, Rolle rolle2, Rolle rolle3) {
         this.rolle1 = rolle1;
         this.rolle2 = rolle2;
         this.rolle3 = rolle3;
     }
 
+    /**
+     * Schickt ein Signal vorwärts durch die drei Rollen, die UmkehrWalze und dann rückwärts durch die drei Rollen
+     * @param eingabe Zeichen, das verschlüsselt werden soll
+     * @return Ergebnis der Verschlüsselung
+     */
     public char verschlüsseln(char eingabe) {
 
         //vorwärts durch alle Rollen
@@ -19,7 +30,7 @@ public class Rollwerk {
         char vorwärts2 = rolle2.vorwärts(vorwärts1);
         char vorwärts3 = rolle3.vorwärts(vorwärts2);
 
-        //Umdrehen in Umkehrwalze
+        //Umdrehen in UmkehrWalze
         char umgedreht = umkehrWalze.reflektieren(vorwärts3);
 
         //Rückwärts durch alle Rollen
@@ -30,6 +41,12 @@ public class Rollwerk {
         return rückwärts1;
     }
 
+    /**
+     * Dreht die Rollen zur nächsten Position
+     * Die erste Rolle wird immer um eine Position gedreht.
+     * Hat die erste Rolle eine Umdrehung zurückgelegt, dreht die zweite Rolle sich um eine Position
+     * Hat die zweite Rolle eine Umdrehung zurückgelegt, dreht die dritte Rolle sich um eine Position
+     */
     public void drehen() {
         rolle1.drehen();
         if (rolle1.getPosition() == 0) {
@@ -40,6 +57,10 @@ public class Rollwerk {
         }
     }
 
+    /**
+     * Gibt die Position der Rollen zurück
+     * @return Ein Array dessen Werte die Position der zugehörigen Rolle angibt. bsp. array[0] → Erste Rolle
+     */
     public int[] getRollenPositionen() {
         return new int[]{
                 rolle1.getPosition(),
@@ -48,6 +69,11 @@ public class Rollwerk {
         };
     }
 
+
+    /**
+     * Setzt die Position der Rollen
+     * @param positionen  Ein Array dessen Werte die Position der zugehörigen Rolle angibt. bsp. array[0] → Erste Rolle
+     */
     public void setRollenPositionen(int[] positionen) {
         if(positionen.length != 3 )
             throw new IllegalArgumentException();
